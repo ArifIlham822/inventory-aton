5233f2008b04a29a9e8377030d7e5b9dbfb14986
ArifIlham822 <ai257115@gmail.com>
2021-04-25 18:38:37 +0700

    First Commit

---
+++ database/migrations/2021_04_25_075842_products.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Products extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->char('name');
+            $table->integer('qyt');
+            $table->char('photo');
+            $table->foreignId('brands_id');
+            $table->foreignId('categories_id');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
